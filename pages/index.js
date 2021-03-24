03b7ff3f9c402af8523ae519b1eb661d8e4d7bdf
Ben Ipsen <email@benipsen.com>
2021-03-23 21:33:47 -0700

    make it a bit more pro

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { Dots } from '../components/dots';
 
 export default function Home() {
 
-  const [party, setParty] = useState(0);
+  const [party, setParty] = useState(20);
 
   return (
     <div className={styles.container}>
@@ -15,26 +15,29 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Dots partyLevel={party} />
-      <main>
+
+      <main className={styles.content}>
         <h1 className={styles.title}>
           Ben Ipsen
         </h1>
         <p>
-          digital craftsperson. fun haver. <br />more content, very soon.  
+          .com! <br /> digital craftsperson. <br />more content, very soon.
         </p>
-        <button onClick={() => setParty(party + 20)}>More Party</button>
-        <button onClick={() => setParty(0)}>Less Party</button>
-        <p><Link href="/fun">Play Asteriods!</Link></p>
+        {/* <button onClick={() => setParty(party + 20)}>More Dots</button>
+        <button onClick={() => setParty(party - 20)}>Less Dots</button> */}
+        <h3><Link href="/fun">Play Asteriods</Link></h3>
         <p><a href="https://github.com/cif/benipsen.com">view source</a></p>
       </main>
-      
+
       <footer className={styles.footer}>
         <p><a href="https://github.com/cif">github</a></p>
         <p><a href="https://linkedin.com/in/benipsen">linkedin</a></p>
         <p><a href="https://twitter.com/benipsen">twitter</a></p>
         <p><a href="mailto:email@benipsen.com?subject=Hello">email</a></p>
       </footer>
-    </div>
+      <div className={styles[`dots-container`]}>
+        <Dots partyLevel={party} />
+      </div>
+    </div >
   )
 }
